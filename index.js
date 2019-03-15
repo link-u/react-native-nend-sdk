24993ec4b5853219b394c7af46468e1f0796fd10
Daniel Schlaug <daniel@schlaug.com>
2019-03-15 14:04:50 +0900

    Add export statements for native views

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
-import { NativeModules } from 'react-native';
+export {default as AdView} from 'src/components/AdView';
+export {default as InterstitialAd} from 'src/components/InterstitialAd';
+export {default as MediaView} from 'src/components/MediaView';
+export {default as NativeAd} from 'src/components/NativeAd';
+export {default as UserFeature} from 'src/components/UserFeature';
+export {default as VideoAd} from 'src/components/VideoAd';
+export {default as VideoNativeAd} from 'src/components/VideoNativeAd';
 
-const { RNReactNativeNendSdk } = NativeModules;
-
-export default RNReactNativeNendSdk;
