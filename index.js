84a694da66ef52c8fe7fddb687a7a45328d5f1e2
Daniel Schlaug <daniel@schlaug.com>
2019-03-15 14:31:14 +0900

    Fix naïve index.js exports

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
 export {default as AdView} from 'src/components/AdView';
 export {default as InterstitialAd} from 'src/components/InterstitialAd';
+import type ClickType from 'src/components/InterstitialAd';
+export type ClickType;
 export {default as MediaView} from 'src/components/MediaView';
-export {default as NativeAd} from 'src/components/NativeAd';
+export {NativeAd, NativeAdLoader} from 'src/components/NativeAd';
+import type AdExplicitly from 'src/components/NativeAd';
+export type AdExplicitly;
 export {default as UserFeature} from 'src/components/UserFeature';
 export {default as VideoAd} from 'src/components/VideoAd';
-export {default as VideoNativeAd} from 'src/components/VideoNativeAd';
-
+import type RewardItem from 'src/components/VideoAd';
+export type RewardItem;
+export {VideoNativeAdLoader} from 'src/components/VideoNativeAd';
+import type VideoNativeAd from 'src/components/VideoNativeAd';
+export type VideoNativeAd;
