88aa7be4918fbbd32995cb27a071e7616b821b76
Daniel Schlaug <daniel@schlaug.com>
2019-03-15 15:42:01 +0900

    Fix Flow type exports

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
+// @flow
 
 export {default as AdView} from 'src/components/AdView';
 export {default as InterstitialAd} from 'src/components/InterstitialAd';
-import type ClickType from 'src/components/InterstitialAd';
-export type ClickType;
+export type {ClickType} from 'src/components/InterstitialAd';
 export {default as MediaView} from 'src/components/MediaView';
 export {NativeAd, NativeAdLoader} from 'src/components/NativeAd';
-import type AdExplicitly from 'src/components/NativeAd';
-export type AdExplicitly;
+export type {AdExplicitly} from 'src/components/NativeAd';
 export {default as UserFeature} from 'src/components/UserFeature';
 export {RewardedVideoAd, InterstitialVideoAd} from 'src/components/VideoAd';
-import type RewardItem from 'src/components/VideoAd';
-export type RewardItem;
+export type {RewardItem} from 'src/components/VideoAd';
 export {VideoNativeAdLoader} from 'src/components/VideoNativeAd';
-import type VideoNativeAd from 'src/components/VideoNativeAd';
-export type VideoNativeAd;
+export type {VideoNativeAd} from 'src/components/VideoNativeAd';
